bf130517aee8e2999b654b6e58b8cb25e157339f
actyp <el17115@mail.ntua.gr>
2022-07-11 14:00:27 +0300

    Fill hasInitialClientMessage

--- src/main/java/gr/ntua/softlab/edhocFuzzer/components/sul/mapper/symbols/EdhocOutputChecker.java
+++ src/main/java/gr/ntua/softlab/edhocFuzzer/components/sul/mapper/symbols/EdhocOutputChecker.java
@@ -3,9 +3,11 @@ package gr.ntua.softlab.edhocFuzzer.components.sul.mapper.symbols;
 import gr.ntua.softlab.protocolStateFuzzer.components.sul.mapper.abstractSymbols.AbstractOutput;
 import gr.ntua.softlab.protocolStateFuzzer.components.sul.mapper.abstractSymbols.AbstractOutputChecker;
 
+import java.util.Objects;
+
 public class EdhocOutputChecker implements AbstractOutputChecker {
     @Override
     public boolean hasInitialClientMessage(AbstractOutput abstractOutput) {
-        return false;
+        return Objects.equals(abstractOutput.getName(), "EDHOC_MESSAGE_1");
     }
 }
