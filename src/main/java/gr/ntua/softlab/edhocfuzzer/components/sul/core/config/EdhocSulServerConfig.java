dc27577bb0a77722b52a856dcbcea46f4da4671a
actyp <actypaldos@gmail.com>
2023-07-10 20:00:52 +0300

    Update config usages

--- src/main/java/gr/ntua/softlab/edhocfuzzer/components/sul/core/config/EdhocSulServerConfig.java
+++ src/main/java/gr/ntua/softlab/edhocfuzzer/components/sul/core/config/EdhocSulServerConfig.java
@@ -1,15 +1,16 @@
 package gr.ntua.softlab.edhocfuzzer.components.sul.core.config;
 
-import com.github.protocolfuzzing.protocolstatefuzzer.components.sul.core.config.SulServerConfig;
+import com.github.protocolfuzzing.protocolstatefuzzer.components.sul.core.config.SulAdapterConfigEmpty;
+import com.github.protocolfuzzing.protocolstatefuzzer.components.sul.core.config.SulServerConfigStandard;
 import com.github.protocolfuzzing.protocolstatefuzzer.components.sul.mapper.config.MapperConfig;
 import com.github.protocolfuzzing.protocolstatefuzzer.components.sul.mapper.config.MapperConnectionConfig;
 import gr.ntua.softlab.edhocfuzzer.components.sul.mapper.config.EdhocMapperConfig;
 import gr.ntua.softlab.edhocfuzzer.components.sul.mapper.config.EdhocMapperConnectionConfig;
 import org.eclipse.californium.elements.config.Configuration;
 
-public class EdhocSulServerConfig extends SulServerConfig {
+public class EdhocSulServerConfig extends SulServerConfigStandard {
     public EdhocSulServerConfig(EdhocMapperConfig edhocMapperConfig) {
-        super(edhocMapperConfig, null);
+        super(edhocMapperConfig, new SulAdapterConfigEmpty());
     }
 
     @Override
