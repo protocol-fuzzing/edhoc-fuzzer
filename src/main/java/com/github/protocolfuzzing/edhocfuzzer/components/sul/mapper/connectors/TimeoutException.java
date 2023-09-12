9597975b7b6a5a70ae5130485429066f029ca360
Kostis Sagonas <kostis@it.uu.se>
2023-09-12 15:59:02 +0200

    Declare a serialVersionUID field (where needed)

--- src/main/java/com/github/protocolfuzzing/edhocfuzzer/components/sul/mapper/connectors/TimeoutException.java
+++ src/main/java/com/github/protocolfuzzing/edhocfuzzer/components/sul/mapper/connectors/TimeoutException.java
@@ -1,5 +1,7 @@
 package com.github.protocolfuzzing.edhocfuzzer.components.sul.mapper.connectors;
 
-@SuppressWarnings("serial")
 public class TimeoutException extends Exception {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
 }
--- src/main/java/com/github/protocolfuzzing/edhocfuzzer/components/sul/mapper/connectors/UnsuccessfulMessageException.java
+++ src/main/java/com/github/protocolfuzzing/edhocfuzzer/components/sul/mapper/connectors/UnsuccessfulMessageException.java
@@ -1,5 +1,7 @@
 package com.github.protocolfuzzing.edhocfuzzer.components.sul.mapper.connectors;
 
-@SuppressWarnings("serial")
 public class UnsuccessfulMessageException extends Exception {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
 }
