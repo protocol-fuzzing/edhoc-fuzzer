27984c3a81ac684754ae5bdce6e10cc30e031d5e
actyp <actypaldos@gmail.com>
2023-11-10 22:41:30 -0500

    Register CoapConfig definitions

--- src/main/java/com/github/protocolfuzzing/edhocfuzzer/components/sul/mapper/config/EdhocMapperConnectionConfig.java
+++ src/main/java/com/github/protocolfuzzing/edhocfuzzer/components/sul/mapper/config/EdhocMapperConnectionConfig.java
@@ -1,15 +1,25 @@
 package com.github.protocolfuzzing.edhocfuzzer.components.sul.mapper.config;
 
 import com.github.protocolfuzzing.protocolstatefuzzer.components.sul.mapper.config.MapperConnectionConfig;
+import org.eclipse.californium.core.config.CoapConfig;
 import org.eclipse.californium.elements.config.Configuration;
 
 import java.io.InputStream;
 
 public class EdhocMapperConnectionConfig implements MapperConnectionConfig {
+    private static boolean registered = false;
+    private static void register() {
+        if (!registered) {
+            CoapConfig.register();
+            registered = true;
+        }
+    }
+
     private Configuration configuration;
 
     public EdhocMapperConnectionConfig(InputStream inputStream) {
-        this.configuration = inputStream == null ? Configuration.getStandard() : Configuration.createFromStream(inputStream, null);
+        register();
+        this.configuration = inputStream == null ? Configuration.getStandard() : Configuration.createStandardFromStream(inputStream);
     }
 
     public Configuration getConfiguration() {
