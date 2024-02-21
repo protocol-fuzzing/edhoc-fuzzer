f05a2873d91a2cc34af021b7d22594c7b1996796
actyp <actypaldos@gmail.com>
2024-02-20 23:12:53 -0500

    Use anonymous classes for empty SulAdapterConfig

--- src/main/java/com/github/protocolfuzzing/edhocfuzzer/components/sul/core/config/EdhocSulServerConfig.java
+++ src/main/java/com/github/protocolfuzzing/edhocfuzzer/components/sul/core/config/EdhocSulServerConfig.java
@@ -2,14 +2,14 @@ package com.github.protocolfuzzing.edhocfuzzer.components.sul.core.config;
 
 import com.github.protocolfuzzing.edhocfuzzer.components.sul.mapper.config.EdhocMapperConfig;
 import com.github.protocolfuzzing.edhocfuzzer.components.sul.mapper.config.EdhocMapperConnectionConfig;
-import com.github.protocolfuzzing.protocolstatefuzzer.components.sul.core.config.SulAdapterConfigEmpty;
+import com.github.protocolfuzzing.protocolstatefuzzer.components.sul.core.config.SulAdapterConfig;
 import com.github.protocolfuzzing.protocolstatefuzzer.components.sul.core.config.SulServerConfigStandard;
 import com.github.protocolfuzzing.protocolstatefuzzer.components.sul.mapper.config.MapperConfig;
 import org.eclipse.californium.elements.config.Configuration;
 
 public class EdhocSulServerConfig extends SulServerConfigStandard {
     public EdhocSulServerConfig(EdhocMapperConfig edhocMapperConfig) {
-        super(edhocMapperConfig, new SulAdapterConfigEmpty());
+        super(edhocMapperConfig, new SulAdapterConfig(){});
     }
 
     @Override
