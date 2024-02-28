9a30c0f3a44b8757d61c1d3dd66882b9acddaa09
00oskpet <42684085+00oskpet@users.noreply.github.com>
2024-02-28 16:11:39 +0100

    Co-authored-by: Linus Olofsson <Kax-y@users.noreply.github.com>

--- scripts/setup_fuzzer.sh
+++ scripts/setup_fuzzer.sh
@@ -22,6 +22,23 @@ setup_psf() {
     set +e
 }
 
+setup_ralib() {
+    # setup RALib
+
+    CHECKOUT="main"
+
+    set -e
+    cd "${BASE_DIR}"
+    git clone "https://github.com/LearnLib/ralib"
+    cd ralib
+    git checkout ${CHECKOUT}
+    mvn install
+
+    cd "${BASE_DIR}"
+    rm -rf ./ralib/
+    set +e
+}
+
 setup_cf_edhoc() {
     # setup cf-edhoc library
 
@@ -68,19 +85,21 @@ usage() {
   Options (library setup prior to EDHOC-Fuzzer):
     -p  Fetch and setup only protocol-state-fuzzer library
     -e  Fetch and setup only cf-edhoc library
-    -l  Fetch and setup protocol-state-fuzzer and cf-edhoc libraries
+    -r  Fetch and setup only ralib
+    -l  Fetch and setup protocol-state-fuzzer, ralib and cf-edhoc libraries
     -h  Show usage message
 END
   exit 0
 }
 
 
-while getopts :pelh flag
+while getopts :pelrh flag
 do
   case "${flag}" in
     p) setup_psf ;;
     e) setup_cf_edhoc ;;
-    l) setup_psf; setup_cf_edhoc ;;
+    r) setup_ralib ;;
+    l) setup_ralib; setup_psf; setup_cf_edhoc ;;
     : | \? | h | *) usage ;;
   esac
 done
