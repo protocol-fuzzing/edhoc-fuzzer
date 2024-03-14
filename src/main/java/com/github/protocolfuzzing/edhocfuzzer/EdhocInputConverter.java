493a8c5cc386bb6115546796b19d3070fdfb359d
00oskpet <42684085+00oskpet@users.noreply.github.com>
2024-03-14 17:05:45 +0100

    pass the parameter values as well in EdhocInputConverter

--- src/main/java/com/github/protocolfuzzing/edhocfuzzer/EdhocInputConverter.java
+++ src/main/java/com/github/protocolfuzzing/edhocfuzzer/EdhocInputConverter.java
@@ -14,31 +14,31 @@ public class EdhocInputConverter implements PSymbolInstanceConverter<EdhocInputR
 
         switch (MessageInputType.valueOf(name)) {
             case EDHOC_MESSAGE_1:
-                return new EdhocMessage1InputRA(base, null);
+                return new EdhocMessage1InputRA(base, input.getParameterValues());
 
             case EDHOC_MESSAGE_2:
-                return new EdhocMessage2InputRA(base, null);
+                return new EdhocMessage2InputRA(base, input.getParameterValues());
 
             case EDHOC_MESSAGE_3:
-                return new EdhocMessage3InputRA(base, null);
+                return new EdhocMessage3InputRA(base, input.getParameterValues());
 
             case EDHOC_MESSAGE_3_OSCORE_APP:
-                return new EdhocMessage3OscoreAppInputRA(base, null);
+                return new EdhocMessage3OscoreAppInputRA(base, input.getParameterValues());
 
             case EDHOC_MESSAGE_4:
-                return new EdhocMessage4InputRA(base, null);
+                return new EdhocMessage4InputRA(base, input.getParameterValues());
 
             case EDHOC_ERROR_MESSAGE:
-                return new EdhocErrorMessageInputRA(base, null);
+                return new EdhocErrorMessageInputRA(base, input.getParameterValues());
 
             case OSCORE_APP_MESSAGE:
-                return new OscoreAppMessageInputRA(base, null);
+                return new OscoreAppMessageInputRA(base, input.getParameterValues());
 
             case COAP_APP_MESSAGE:
-                return new CoapAppMessageInputRA(base, null);
+                return new CoapAppMessageInputRA(base, input.getParameterValues());
 
             case COAP_EMPTY_MESSAGE:
-                return new CoapEmptyMessageInputRA(base, null);
+                return new CoapEmptyMessageInputRA(base, input.getParameterValues());
 
             default:
                 throw new RuntimeException("The name '" + name + "' of the " + PSymbolInstance.class.getName()
