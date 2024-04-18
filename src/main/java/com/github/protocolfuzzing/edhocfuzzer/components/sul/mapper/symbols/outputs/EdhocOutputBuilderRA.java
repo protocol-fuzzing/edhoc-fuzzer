f6b3e07e3f5fd204bbc2401bb9df3aafabeda575
Oskar Pettersson Löfstedt <00oskpet@gmail.com>
2024-04-18 16:44:01 +0200

    Update the OutputBuilder to build PSymbolInstances from MessageOutputType members
    Co-authored-by: Linus Olofsson <Kax-y@users.noreply.github.com>

--- src/main/java/com/github/protocolfuzzing/edhocfuzzer/components/sul/mapper/symbols/outputs/EdhocOutputBuilderRA.java
+++ src/main/java/com/github/protocolfuzzing/edhocfuzzer/components/sul/mapper/symbols/outputs/EdhocOutputBuilderRA.java
@@ -1,11 +1,39 @@
 package com.github.protocolfuzzing.edhocfuzzer.components.sul.mapper.symbols.outputs;
 
 import com.github.protocolfuzzing.protocolstatefuzzer.components.sul.mapper.abstractsymbols.OutputBuilder;
+import de.learnlib.ralib.words.OutputSymbol;
 import de.learnlib.ralib.words.PSymbolInstance;
 
 public class EdhocOutputBuilderRA implements OutputBuilder<PSymbolInstance> {
+
     @Override
     public PSymbolInstance buildOutput(String name) {
-        return new EdhocOutputRA(name);
+        OutputSymbol baseSymbol = new OutputSymbol(name);
+        return new PSymbolInstance(baseSymbol);
+    }
+
+    public PSymbolInstance buildOutput(MessageOutputType type) {
+        OutputSymbol baseSymbol = new OutputSymbol(type.name());
+        return new PSymbolInstance(baseSymbol);
+    }
+
+    @Override
+    public PSymbolInstance buildTimeout() {
+        return buildOutput(MessageOutputType.TIMEOUT);
+    }
+
+    @Override
+    public PSymbolInstance buildUnknown() {
+        return buildOutput(MessageOutputType.UNKNOWN);
+    }
+
+    @Override
+    public PSymbolInstance buildSocketClosed() {
+        return buildOutput(MessageOutputType.SOCKET_CLOSED);
+    }
+
+    @Override
+    public PSymbolInstance buildDisabled() {
+        return buildOutput(MessageOutputType.DISABLED);
     }
 }
