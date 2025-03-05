b3190ebaa58dfb9d19ecd004a96e20a4f4d18e21
Kostis Sagonas <kostis@it.uu.se>
2025-03-05 13:32:44 +0200

    Bump lakers version from 0.7.2 to 0.8.0

--- scripts/setup_sul.sh
+++ scripts/setup_sul.sh
@@ -14,7 +14,7 @@ mkdir -p "${SOURCES_DIR}" "${SERVERS_DIR}" "${CLIENTS_DIR}"
 
 setup_lakers() {
   # lakers
-  CHECKOUT="tags/v0.7.2"
+  CHECKOUT="tags/v0.8.0"
 
   set -e
   echo "Setting up Lakers in ${SOURCES_DIR}"
