1017757e8e7094beb782dcb2650ddd1a1845e56d
Oskar Pettersson Löfstedt <00oskpet@gmail.com>
2026-01-15 17:01:02 +0100

    Update psf version to the same as mainline edhocfuzzer

--- scripts/setup_fuzzer.sh
+++ scripts/setup_fuzzer.sh
@@ -8,7 +8,7 @@ readonly BASE_DIR
 setup_psf() {
     # setup protocol-state-fuzzer library
 
-    CHECKOUT="2ec09644043e66b9f5139b18d489763c4fc0e8ec"
+    CHECKOUT="42392348464ab4cebb3a4c7b149c21caf6ee9c4e"
 
     set -e
     cd "${BASE_DIR}"
